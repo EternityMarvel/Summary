3e24d699fd7d25e5007bac4dfe79dbde95776f8c
EternityMarvel <574556040@qq.com>
2017-03-06 14:53:05 +0800

    Create UserAction.java

---
+++ SSH/annotation/UserAction.java
@@ -0,0 +1,44 @@
+import javax.annotation.Resource;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.Results;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.menghao.ssh.entity.User;
+import com.menghao.ssh.service.UserService;
+
+
+/**
+ * @Description:
+ * @Date: 2017/3/6 15:04
+ * @Author: Mr.m
+ */
+@Results( { @Result(name="success",location="/success.jsp"),
+        @Result(name="failure",location="/failure.jsp") }) 
+public class UserAction extends ActionSupport {
+    @Resource
+    private UserService userService;
+    private User user;
+    
+    @Action(value="save")
+    public String addUser() {
+        try {
+            userService.save(user);        
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "failure";
+        }
+        return "success";
+        
+    }    
+
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+    
+    
+}
